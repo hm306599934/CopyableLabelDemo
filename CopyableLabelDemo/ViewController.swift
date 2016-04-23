44ff5b1926836550c873adfbd4b555cb25f2134f
Jimmy <Jimmy@triers-Mac-mini.local>
2016-04-23 16:13:04 +0800

    das

---
+++ CopyableLabelDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CopyableLabelDemo
+//
+//  Created by Jimmy on 23/4/2016.
+//  Copyright © 2016 Jimmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
